104c39d32df3aa08aa64b1d005067c8972e48e7f
HAYOUNG <parkhy4108@gmail.com>
2023-02-27 03:57:43 +0900

    20230227

--- 단어변환.kt
+++ 단어변환.kt
@@ -1,21 +1,32 @@
-//fun main(){
-//    val begin = "hit"
-//    val target = "cog"
-//    val words = arrayOf("hot", "dot", "dog", "lot", "log", "cog")
-//    Solution().solution(begin, target, words)
-//}
-//
-//class Solution {
-//    fun solution(begin: String, target: String, words: Array<String>): Int {
-//        var answer = 0
-//        var cnt = 0
-//        if (words.contains(target)) {
-//            words.forEach { word ->
-//                if(target != word) cnt++
-//                else answer = cnt
-//            }
-//        } else answer = 0
-//        println(answer)
-//        return answer
-//    }
-//}
+import kotlin.math.min
+
+class WordChange {
+    var min = Int.MAX_VALUE
+    fun solution(begin: String, target: String, words: Array<String>): Int {
+        if (!words.contains(target)) return 0
+        else dfs(begin, target, visited = BooleanArray(words.size), words, 0)
+        if(min == Int.MAX_VALUE) return 0
+        println(min)
+        return min
+    }
+
+    private fun dfs(begin: String, target: String, visited: BooleanArray, words: Array<String>, cnt: Int) {
+        if (begin == target) min = min(min, cnt)
+        else {
+            for (i in words.indices) {
+                var c = 0
+                for (j in begin.indices) if (begin[j] == words[i][j]) c++
+                if (!visited[i] && c == begin.length - 1) {
+                    visited[i] = true
+                    println(words[i])
+                    dfs(words[i], target, visited, words, cnt + 1)
+                    visited[i] = false
+                }
+            }
+        }
+    }
+}
+
+/*
+
+ */
