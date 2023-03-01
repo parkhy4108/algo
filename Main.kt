f32e0ba2fe86e6ac7dfece67bad1c8f04ede7690
HAYOUNG <parkhy4108@gmail.com>
2023-03-02 03:23:00 +0900

    20230302

--- Main.kt
+++ Main.kt
@@ -1,6 +1,18 @@
-fun main(){
-    val begin = "hit"
-    val target = "cog"
-    val words = arrayOf("hot", "dot", "dog", "lot", "log", "cog")
-    WordChange().solution(begin,target,words)
+fun main() {
+    val n = 7
+    val edge = arrayOf(
+        intArrayOf(3,6),
+        intArrayOf(4,3),
+        intArrayOf(3,2),
+        intArrayOf(1,3),
+        intArrayOf(1,2),
+        intArrayOf(2,4),
+        intArrayOf(5,2),
+        intArrayOf(5,7)
+
+//        intArrayOf(1,4),
+//        intArrayOf(4,3),
+//        intArrayOf(1,2)
+    )
+    Solution().solution(n, edge)
 }
